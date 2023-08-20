b66c4a395aaaf1e8f568bf40a1300631fb05c9c7
Mohammed Shekha <msh@odoo.com>
2023-08-20 23:09:48 +0530

    [IMP] Improve the basic structure of application with header, body and footer

--- auction/static/src/auction.js
+++ auction/static/src/auction.js
@@ -1,7 +1,13 @@
 /** @odoo-module **/
 
 import { Component } from "@odoo/owl";
+import { Header } from "./components/Header/header";
+import { Body } from "./components/Container/body";
+import { Footer } from "./components/Footer/footer";
+
 
 export class Auction extends Component {
     static template = "auction.root";
 }
+
+Auction.components = { Header, Body, Footer }
---
+++ auction/static/src/components/Container/body.js
@@ -0,0 +1,7 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Body extends Component {
+    static template = "auction.body";
+}
