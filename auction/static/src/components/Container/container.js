085d3450b7df4d6e0c16ef7332986967a0af68ee
Mohammed Shekha <msh@odoo.com>
2023-08-25 19:42:15 +0530

    [IMP] Add props and defaultProps for props validation and detaul properties

--- auction/static/src/components/Container/container.js
+++ auction/static/src/components/Container/container.js
@@ -11,3 +11,13 @@ export class Container extends Component {
 }
 
 Container.components = { AuctionListContainer, AuctionDetails };
+
+Container.props = {
+    mainScreenProps: { type: Object, optional: true },
+    tname: { type: String },
+    component: { type: Function }
+};
+
+Container.defaultProps = {
+    mainScreenProps: {},
+};
